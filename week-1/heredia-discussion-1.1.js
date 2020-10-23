e6e4ec38bd207f80df7559ead7b4ace44c518b3d
Daniel Heredia <mrheredia817@gmail.com>
2020-10-23 17:17:16 -0400

    completion of discussion 1.1 and assigment 1.2

---
+++ week-1/heredia-discussion-1.1.js
@@ -0,0 +1,15 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Daniel Heredia
+; Date: 22 October 2020
+; Modified By: Daniel Heredia
+; Description: This program demonstrates the
+; use of JavaScript values and variables with
+; at least two errors.
+;===========================================
+*/
+
+var current = new Date(); //Declares variable current to the current date and time
+var text = "This class is going to be very challenging;
+console.log current + text //Displays date and text on the page
