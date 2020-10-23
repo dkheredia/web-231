b9f7fb2d6d0bb7e70fb6e50e2e4651e746ad0d7e
Daniel Heredia <mrheredia817@gmail.com>
2020-10-23 17:11:51 -0400

    completion of assignment 1.3

---
+++ week-1/hello-world.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Daniel Heredia
+; Date: 23 October 2020
+; Modified By: Daniel Heredia
+; Description: This is a basic JavaScript
+; program of Hello World
+;===========================================
+*/
+
+console.log("Hello World, my name is Daniel Heredia");
