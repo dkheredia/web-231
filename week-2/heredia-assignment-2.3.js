9bd7dac421d8b3c1eeead98fcc9f3140732179b2
Daniel Heredia <mrheredia817@gmail.com>
2020-11-01 23:28:44 -0500

    Updated assignement 2.3

--- week-2/heredia-assignment-2.3.js
+++ week-2/heredia-assignment-2.3.js
@@ -1,5 +1,43 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso
+; Date:   30 October 2020
+; Modified By: Daniel Heredia
+; Description: Displays a formatted header
+;===========================================
+*/
+
+/**
+* Params: none
+* Response: output
+* Description: Returns a well-formatted string header
+*/
+
 const header = require('/Users/dhered1/Desktop/backup/repos/github/bu-webdev/web-231/week-2/heredia-header.js');
 
 console.log(header.display("Daniel", "Heredia", "Assignment 2.3 Function Properties"));
+
+
+// function property definition
+myName.daniel = "Daniel"
+
+
+/**
+* Params: none
+* Response: Display my first name
+* Description: Returns the value assigned to myName.daniel
+*/
+
+function myName () {
+  return myName.daniel;
+}
+
+
+//new line
+console.log('\n');
+
+//output
+console.log(myName());
 
 
