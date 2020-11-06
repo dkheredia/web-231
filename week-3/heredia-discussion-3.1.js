6ffc49e4acf2fa4f88d7ca6e201f24f6af523aaf
Daniel Heredia <mrheredia817@gmail.com>
2020-11-05 21:17:06 -0500

    completed discussion 3.1

---
+++ week-3/heredia-discussion-3.1.js
@@ -0,0 +1,36 @@
+/*
+============================================
+; Title:  heredia-discussion-3.1.js
+; Author: Professor Krasso
+; Date:   05 November 2020
+; Modified By: Daniel Heredia
+; Description: Create simple program using if
+; else with at least two errors
+;===========================================
+*/
+
+/**
+* Params: none
+* Response: output
+* Description: Returns a well-formatted string header
+*/
+const header = require('/Users/dhered1/Desktop/backup/repos/github/bu-webdev/web-231/week-2/heredia-header.js');
+console.log(header.display("Daniel", "Heredia", "Assignment 2.4"));
+
+
+// variable declaration and assignment
+let x = new Date()
+
+// conditional statement
+if x.getHours() >= 6 && getHours() < 9
+
+//output
+  console.log("Time to get the day started!");
+
+// conditional statement
+else
+
+//output
+  console.log("Hope you had a great day learning to code");
+
+
